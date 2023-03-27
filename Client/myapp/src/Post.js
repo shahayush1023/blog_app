df143270a8163ee3b2683b87000d24f7779c4d52
Ayush Shah <shahayush329@gmail.com>
2023-03-27 19:44:22 +0530

    second commit

---
+++ Client/myapp/src/Post.js
@@ -0,0 +1,20 @@
+export default function Post(){
+    return (
+        <div className="entry">
+        <div className="image">
+          <img
+            src="https://static.toiimg.com/photo/msid-69955935/69955935.jpg"
+            alt="ms dhoni"
+          />
+        </div>
+        <div className="texts">
+          <h2>A captain who won all icc trophies💪</h2>
+          <p className="info">
+            <a className="author">AYUSH SHAH</a>
+            <time>2023-10-01 16:30</time>
+          </p>
+          <p className="summary">kvwer ervwe woeoowe oweowegoweooglwegwejg we wej w</p>
+        </div>
+      </div>
+    );
+}
